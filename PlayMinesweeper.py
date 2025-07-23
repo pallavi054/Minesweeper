37253ec7b314278c780c32a960b39554e7bef740
Ojan Salemi <ojansalemi@gmail.com>
2025-07-23 22:48:51 +0330

    implemented GameLogic.py and PlayMinesweeper.py

--- PlayMinesweeper.py
+++ PlayMinesweeper.py
@@ -10,24 +10,14 @@ from MinesweeperBoard import *
 # Create a Minesweeper board
 # game = MinesweeperBoard(level='expert')
 # game = MinesweeperBoard(level='beginner')
-game = MinesweeperBoard(level='intermediate')
+#game = MinesweeperBoard(level='intermediate')
 
-# Start the game timer
-game.start_timer()
 
-# Display the board
-game.display_board()
+ from GameLogic import *
 
-# =============================================================================
-# TO CALL GAMELOGIC CLASS AFTER IT'S DONE
-# from GameLogic import *
-# 
-# def main():
-#     print("Welcome to Console Minesweeper!")
-#     print("Use 'R row col' to reveal a cell, 'F row col' to place a flag, or 'N' to reset the game.\n")
-# 
-#     GameLogic()  # start the game
-# 
-# if __name__ == "__main__":
-#     main()
-# =============================================================================
+ def main():
+     game = GameLogic()  # start the game
+     game.run()
+
+ if __name__ == "__main__":
+     main()
